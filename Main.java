b54edabfbb931828302699d0f60a87f2ee8162d0
LucasWitka <s36008@pjwstk.edu.pl>
2026-03-07 13:02:23 +0100

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,10 @@
+//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
+// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+public class Main {
+    public static void main(String[] args) {
+        int a = 20;
+        int b = 3;
+        int sum = (a * b);
+        System.out.print(sum);
+    }
+}
