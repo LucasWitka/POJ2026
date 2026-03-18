f2fd20549a367348341c6c4f72a27e2a05798615
LucasWitka <s36008@pjwstk.edu.pl>
2026-03-18 22:35:06 +0100

    Add files via upload

---
+++ Zadania_07-08.03.26/Main.java
@@ -0,0 +1,10 @@
+
+import java.util.Scanner;
+
+public void main(String[] args) {
+    double a, b;
+    Scanner sc = new Scanner(System.in);
+    a = sc.nextDouble();
+    b = sc.nextDouble();
+    System.out.println(a*b);
+}
