80a4bf52c34eb64adcbe255ca5933879987dbd98
LucasWitka <s36008@pjwstk.edu.pl>
2026-04-24 09:40:22 +0200

    Add files via upload

---
+++ Zadania_11-12.04.26/Zad1/Maszyna.java
@@ -0,0 +1,17 @@
+public abstract class Maszyna {
+
+    protected String marka;
+    protected String nazwa;
+    protected double pojemnoscSilnika;
+    protected RodzajSilnika rodzajSilnika;
+
+    public Maszyna(String marka, String nazwa, double pojemnoscSilnika, RodzajSilnika rodzajSilnika) {
+        this.marka = marka;
+        this.nazwa = nazwa;
+        this.pojemnoscSilnika = pojemnoscSilnika;
+        this.rodzajSilnika = rodzajSilnika;
+    }
+
+    public abstract void wyswietl();
+
+}
---
+++ Zadania_11-12.04.26/Zad1/RodzajSilnika.java
@@ -0,0 +1,7 @@
+public enum RodzajSilnika {
+    BRAK,
+    SPALINOWY,
+    HYBRYDOWY,
+    ELEKTRYCZNY,
+
+}
