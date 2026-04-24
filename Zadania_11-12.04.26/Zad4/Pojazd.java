7704c88323cdaf6e62a7ca37a92aba7a85f15791
LucasWitka <s36008@pjwstk.edu.pl>
2026-04-24 09:44:10 +0200

    Add files via upload

---
+++ Zadania_11-12.04.26/Zad4/Pojazd.java
@@ -0,0 +1,27 @@
+public class Pojazd extends Maszyna {
+
+    protected double moc;
+    protected double momentObrotowy;
+
+    protected int nrPojazdu;
+    protected static int maxLiczbaPojazdow = 0;
+
+    public Pojazd(String marka, String nazwa, double pojemnoscSilnika,
+                  RodzajSilnika rodzajSilnika,
+                  double moc, double momentObrotowy) {
+        super(marka, nazwa, pojemnoscSilnika, rodzajSilnika);
+        this.moc = moc;
+        this.momentObrotowy = momentObrotowy;
+        maxLiczbaPojazdow++;
+        this.nrPojazdu = maxLiczbaPojazdow;
+    }
+
+    @Override
+    public void wyswietl() {
+        System.out.println("Pojazd " + nrPojazdu + " z " + maxLiczbaPojazdow);
+        System.out.println("Marka: " + marka);
+        System.out.println("Nazwa: " + nazwa);
+        System.out.println("Moc: " + moc);
+        System.out.println("Moment obrotowy: " + momentObrotowy);
+    }
+}
