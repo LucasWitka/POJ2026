4f173c7b5ca75d9254393e090292b9d05c4c3b1f
LucasWitka <s36008@pjwstk.edu.pl>
2026-04-24 09:45:12 +0200

    Add files via upload

---
+++ Zadania_11-12.04.26/Zad5/Samochod.java
@@ -0,0 +1,22 @@
+public class Samochod extends Pojazd {
+
+    private Segment segment;
+    private String VIN;
+
+    public Samochod(String marka, String nazwa, double pojemnoscSilnika,
+                    RodzajSilnika rodzajSilnika,
+                    double moc, double momentObrotowy,
+                    Segment segment, String VIN) {
+        super(marka, nazwa, pojemnoscSilnika, rodzajSilnika, moc, momentObrotowy);
+        this.segment = segment;
+        this.VIN = VIN;
+    }
+
+    @Override
+    public void wyswietl() {
+        System.out.println("Samochod " + nrPojazdu + " z " + maxLiczbaPojazdow);
+        System.out.println("Marka: " + marka);
+        System.out.println("Segment: " + segment);
+        System.out.println("VIN: " + VIN);
+    }
+}
---
+++ Zadania_11-12.04.26/Zad5/TypJednosladu.java
@@ -0,0 +1,6 @@
+public enum TypJednosladu {
+    MOTOCYKL,
+    SKUTER,
+    ROWER,
+    HULAJNOGA
+}
